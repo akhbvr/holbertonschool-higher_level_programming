b7e0f9143936debe3fd19fd09a00dabbdb1b1ac0
Akbar <akhbvr@gmail.com>
2025-11-20 15:04:26 +0000

    update file

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-import sys
+
+if __name__ == "__main__":
+    import sys
 
 
-argument_length = len(sys.argv) - 1
+    argument_length = len(sys.argv) - 1
 
-if argument_length == 0:
-    print("0 arguments.")
-elif argument_length > 0:
-    print("{} arguments:".format(argument_length))
-    for i in range(1,len(sys.argv)):
-        print(i, sys.argv[i])
+    if argument_length == 0:
+        print("0 arguments.")
+    elif argument_length > 0:
+        print("{} arguments:".format(argument_length))
+        for i in range(1,len(sys.argv)):
+            print(i, sys.argv[i])
 
