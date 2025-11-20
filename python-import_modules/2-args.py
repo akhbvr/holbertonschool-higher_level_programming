6e70151463e3674d9add8f86f6f1bf66091313e4
Akbar <akhbvr@gmail.com>
2025-11-20 15:21:01 +0000

    fix some bug

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
-
 
     argument_length = len(sys.argv) - 1
 
     if argument_length == 0:
         print("0 arguments.")
-    elif argument_length > 0:
+    elif argument_length == 1:
+        print("{} argument:".format(argument_length))
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
         print("{} arguments:".format(argument_length))
-        for i in range(1,len(sys.argv)):
+        for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
 
