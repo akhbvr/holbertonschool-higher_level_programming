e3648c363aa08d75b0ab8d0530d7f89b7d4db980
Akbar <akhbvr@gmail.com>
2025-11-21 15:23:33 +0000

    update 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list: list, idx: int):
-    if idx < 0 and (len(my_list) - 1) < idx:
+    if idx < 0 or (len(my_list) - 1) < idx:
         return None
     return my_list[idx]
