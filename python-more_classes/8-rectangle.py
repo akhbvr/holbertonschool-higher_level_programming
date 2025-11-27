011d7865ba052fc9aa5c6b29a2facfd20bc9dcf3
Akbar <akhbvr@gmail.com>
2025-11-27 15:59:08 +0000

    delete type checking in function argument

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -75,7 +75,7 @@ class Rectangle:
         print('Bye rectangle...')
 
     @staticmethod
-    def bigger_or_equal(rect_1: "Rectangle", rect_2: "Rectangle") -> "Rectangle":
+    def bigger_or_equal(rect_1, rect_2) -> "Rectangle":
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
