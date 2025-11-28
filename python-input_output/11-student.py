9e74f266acc5df0c44e0534cf628b62ee6908e2d
Akbar <akhbvr@gmail.com>
2025-11-28 23:12:08 +0000

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return json data of object"""
+
+        is_valid_attrs = (
+            isinstance(attrs, list) and
+            all(isinstance(x, str) for x in attrs)
+        )
+        if is_valid_attrs:
+            result = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    result[key] = self.__dict__[key]
+            return result
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+
+        for key, value in json.items():
+            self.__dict__[key] = value
