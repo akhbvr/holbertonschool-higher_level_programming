8c0fe7fe38522e2f004989d47828c48dfebbe3bd
Akbar <akhbvr@gmail.com>
2025-11-28 18:10:26 +0000

    Square #1

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Improve Geometry"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Rectangle"""
+
+    def __init__(self, size):
+        """define width and height of rectangle"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """calculate rectangle area"""
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """override str method"""
+
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
