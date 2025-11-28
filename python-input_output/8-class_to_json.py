e201afb48b521063af6303ea7c2819f19fc2d17f
Akbar <akhbvr@gmail.com>
2025-11-28 22:00:17 +0000

    delete modules

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,14 +2,7 @@
 """Class to JSON"""
 
 
-FirstClass = __import__('8-my_class').MyClass
-SecondClass = __import__('8-my_class_2').MyClass
-
-
 def class_to_json(obj):
     """class to json"""
 
-    if isinstance(obj, FirstClass) or isinstance(obj, SecondClass):
-        return obj.__dict__
-
-    return
+    return obj.__dict__
