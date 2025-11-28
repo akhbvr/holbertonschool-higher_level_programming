8a7d7c895488d9a9e65224bc910aec14c240337a
Akbar <akhbvr@gmail.com>
2025-11-28 17:44:56 +0000

    Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Improve Geometry"""
+
+
+class BaseGeometry:
+    """Improve Geometry"""
+
+    def area(self):
+        """raises an Exception"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle"""
+
+    def __init__(self, width, height):
+        """define width and height of rectangle"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
