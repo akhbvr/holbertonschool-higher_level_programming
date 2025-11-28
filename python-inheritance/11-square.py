f3106fd3b4ba493c3f741607a4b55b9b4eb346f2
Akbar <akhbvr@gmail.com>
2025-11-28 18:13:47 +0000

    update current file

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -22,4 +22,4 @@ class Square(Rectangle):
     def __str__(self):
         """override str method"""
 
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+        return "[Square] {}/{}".format(self.__size, self.__size)
