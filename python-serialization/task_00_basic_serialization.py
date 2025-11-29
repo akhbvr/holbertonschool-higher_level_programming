affe9c82aa92c3ff0a10ea544c7cdfb6b3930335
Akbar <akhbvr@gmail.com>
2025-11-29 01:02:42 +0000

    Basic Serialization

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Basic Serialization"""
+
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """serialize and save data to the specified file"""
+
+    with open(filename, "w") as f:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    """load and deserialize data from the specified file"""
+
+    with open(filename, "r") as f:
+        return json.load(f)
