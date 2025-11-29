14b15b7287dbc4c5e4240ab236cec48c38f49611
Akbar <akhbvr@gmail.com>
2025-11-29 02:22:51 +0000

    Serializing and Deserializing with XML

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Serializing and Deserializing with XML"""
+
+
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """serialize the dictionary into XML file"""
+
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, str(key))
+        child.text = str(value)
+
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+
+def deserialize_from_xml(filename):
+    """read the XML data and return a deserialized py dict"""
+
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    result = {}
+
+    for child in root:
+        text = child.text
+        if text is None:
+            value = None
+        else:
+            lower = text
+            if lower == "true":
+                value = True
+            elif lower == "false":
+                value = False
+            else:
+                try:
+                    value = int(text)
+                except ValueError:
+                    try:
+                        value = float(text)
+                    except ValueError:
+                        value = str(text)
+        result[child.tag] = value
+    return result
