80068a3294c1478d4da8b16cbc5d67b6127ca82a
Akbar <akhbvr@gmail.com>
2025-11-29 00:46:24 +0000

    delete white space

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -16,7 +16,7 @@ def pascal_triangle(n):
         new = []
         triangle.append([1])
         triangle.append([1, 1])
-    
+ 
         for i in range(3, n + 1):
             for j in range(0, i - 1):
                 if new == []:
